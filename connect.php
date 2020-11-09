c84cff455803a72de38896905a53f8ed9948ce0a
Alp4c4 <74106021+Alp4c4@users.noreply.github.com>
2020-11-09 18:34:35 +0700

    .

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO("mysql:host=localhost; dbname=sss; charset=utf8", "root", "");
+$pdo = new PDO("mysql:host=localhost; dbname=student; charset=utf8", "root", "");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
