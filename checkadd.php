2f2c7767aa58e260a38c86c911f97d62d0f8c799
MAX1MV5 <74127288+MAX1MV5@users.noreply.github.com>
2020-11-10 18:48:22 +0700

    เปลี่ยนรหัสผ่าน

---
+++ checkadd.php
@@ -0,0 +1,30 @@
+<!-- ไม่ใช้ไฟล์นี้ -->
+<?php 
+include "connect.php";
+session_start();
+// $course_id = $_POST["course_id"];
+$stmt = $pdo->prepare("SELECT register.course_id,course.title FROM register JOIN course ON register.course_id = course.course_id WHERE std_id = ? AND course.course_id = ?");
+$stmt->bindParam(1,$_SESSION["std_id"]);
+$stmt->bindParam(2,$_POST["course_id"]);
+$stmt->execute();
+$row = $stmt->fetch();
+if(empty($row)){
+    $stm = $pdo->prepare("SELECT * FROM course WHERE course_id = ?");
+    $stm->bindParam(1,$_POST["course_id"]);
+    $stm->execute();
+    $ro = $stm->fetch();
+    if(!empty($ro)){
+        echo "<script>
+                var ans = confirm('ต้องการลงทะเบียนวิชา ".$ro["title"]."');
+                if(ans==true)document.location = 'addsub.php?course_id=".$_POST["course_id"]."';
+            </script>";
+        header("location:addregistration.php");
+            
+    }else{
+    }
+
+}else{
+    echo "<script>alert('เคยลงทะเบียนวิชานี้แล้ว'); </script>";
+    header("location:addregistration.php");
+}
+?>
