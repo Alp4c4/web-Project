8ea1219ee913fc41d412ca0cbddc0e1e696ee899
Alp4c4 <74106021+Alp4c4@users.noreply.github.com>
2020-11-10 20:05:46 +0700

    std

--- std_id.php
+++ std_id.php
@@ -1,3 +1,8 @@
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
 <?php
 include "connect.php";
 $stmt = $pdo->prepare('SELECT * FROM `student` WHERE std_id = ?');
@@ -5,13 +10,16 @@ $stmt->bindParam(1,$_GET["std_id"]);
 $stmt->execute();
 $row = $stmt->fetch();
 if(empty($row)){
-    // echo "<script>alert('ไม่พบรหัสนี้'); </script>";
+     echo "<script>alert('ไม่พบรหัสนี้'); </script>";
 }else{
+    echo"<table><tr><th>ชื่อ-นามสกุล</th><th>เลขที่ประจำตัว</th><th>จังหวัดภูมิลำเนา</th></tr>";
     $name = $row["std_name"];
     $id = $row["std_id"];
     $prov = $row["province"];
 
-    echo $name." ".$id." ".$prov;
+    echo "<tr><td>".$name."</td><td>" .$id."</td><td>".$prov."</td></tr>";
+    echo"</table>";
+    
 }
 
 ?>
