bb399f652c63fbb985925ed927e5016aa5219672
Alp4c4 <74106021+Alp4c4@users.noreply.github.com>
2020-11-10 20:53:07 +0700

    Update std_id.php

--- std_id.php
+++ std_id.php
@@ -10,7 +10,7 @@ $stmt->bindParam(1,$_GET["std_id"]);
 $stmt->execute();
 $row = $stmt->fetch();
 if(empty($row)){
-     echo "<script>alert('ไม่พบรหัสนี้'); </script>";
+   
 }else{
     echo"<table><tr><th>ชื่อ-นามสกุล</th><th>เลขที่ประจำตัว</th><th>จังหวัดภูมิลำเนา</th></tr>";
     $name = $row["std_name"];
