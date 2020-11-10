5f4950092ad3917f7ff28c443e3e5d17a7db908d
Alp4c4 <74106021+Alp4c4@users.noreply.github.com>
2020-11-10 20:54:48 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,50 +2,56 @@
 include "connect.php";
 session_start();
 
-$stmt = $pdo->prepare("SELECT * FROM login JOIN student WHERE student.std_id = ? AND login.password = ?");
-$stmt->bindParam(1,$_POST["username"]);
-$stmt->bindParam(2,$_POST["password"]);
-$stmt->execute();
-$row = $stmt->fetch();
-
-if(!empty($row)){
-    session_regenerate_id();
-    $_SESSION["std_id"] = $row["std_id"];
-    $_SESSION["std_name"] = $row["std_name"];
-
-    echo "
-    <!DOCTYPE html>
-<html lang='en'>
+?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Registration</title>
-    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        function confirmDelete(course_id){
+            var ans = confirm("ต้องการถอนรหัสวิชา "+course_id);
+            if(ans==true)document.location = "delete.php?course_id="+course_id;
+        }
+    </script>
 </head>
 <body>
-<div class=validlogin>
-<p>เข้าสู่ระบบสำเร็จ</p>
-</div>
-";
-    echo "<a href='index.html'>test</a>";
+    <?php
+    $stmt = $pdo->prepare("SELECT * FROM login JOIN student WHERE student.std_id = ? AND login.std_id = ? AND login.password = ?");
+    $stmt->bindParam(1,$_POST["username"]);
+    $stmt->bindParam(2,$_POST["username"]);
+    $stmt->bindParam(3,$_POST["password"]);
+    $stmt->execute();
+    $row = $stmt->fetch();
     
+    if(!empty($row)){
+        session_regenerate_id();
+        $_SESSION["std_id"] = $_POST["username"];
+        $_SESSION["std_name"] = $row["std_name"];
+    
+        // echo "เข้าสู่ระบบสําเร็จ<br>";
+        // echo "<a href='index.html'>test</a>";
 
-    // header("location:index.html");
-}else{
-    echo     "<!DOCTYPE html>
-    <html lang='en'>
-    <head>
-        <meta charset='UTF-8'>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <title>Registration</title>
-        
-    </head>
-    <body>
-    <div>
-    <p>เข้าสู่ระบบไม่สำเร็จ</p>
-    </div>
-    ";
-    echo "<a href='login.html'>เข้าสู่ระบบอีกครั้ง</a>";
-}
+        echo "<script>
+                var ans = confirm('เข้าสู่ระบบสําเร็จ')
+                if(ans==true)document.location = 'index.html'
+            </script>";
 
-?>
+    
+        // header("location:index.html");
+    }else{
+        // echo "ไม่สำเร็จ";
+        // echo "<a href='login.html'>เข้าสู่ระบบอีกครั้ง</a>";
+
+        echo "<script>
+                var ans = confirm('เข้าสู่ระบบไม่สําเร็จ')
+                if(ans==true)document.location = 'login.html'
+            </script>";
+    }
+
+
+    ?>
+    
+</body>
+</html>
