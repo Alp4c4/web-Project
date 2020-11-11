fcf50d36d657d9144a89ebd66c9642686d4093f7
Alp4c4 <74106021+Alp4c4@users.noreply.github.com>
2020-11-11 14:27:16 +0700

    .

---
+++ n/std_id.php
@@ -0,0 +1,27 @@
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+<?php
+include "connect.php";
+$stmt = $pdo->prepare('SELECT * FROM `student` WHERE std_id = ?');
+$stmt->bindParam(1,$_GET["std_id"]);
+$stmt->execute();
+$row = $stmt->fetch();
+if(empty($row)){
+    echo '<script language="javascript">';
+    echo 'alert(ไม่พบรหัสนี้)';
+    echo '</script>';
+}else{
+    echo"<table><tr><th>ชื่อ-นามสกุล</th><th>เลขที่ประจำตัว</th><th>จังหวัดภูมิลำเนา</th></tr>";
+    $name = $row["std_name"];
+    $id = $row["std_id"];
+    $prov = $row["province"];
+
+    echo "<tr><td>".$name."</td><td>" .$id."</td><td>".$prov."</td></tr>";
+    echo"</table>";
+    
+}
+
+?>
