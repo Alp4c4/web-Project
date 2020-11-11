4c5e65576dc41cac98cc39947ebb122b4d42983f
Alp4c4 <74106021+Alp4c4@users.noreply.github.com>
2020-11-11 14:10:54 +0700

    .

--- std_id.php
+++ std_id.php
@@ -10,7 +10,9 @@ $stmt->bindParam(1,$_GET["std_id"]);
 $stmt->execute();
 $row = $stmt->fetch();
 if(empty($row)){
-   
+    echo '<script language="javascript">';
+    echo 'alert(ไม่พบรหัสนี้)';
+    echo '</script>';
 }else{
     echo"<table><tr><th>ชื่อ-นามสกุล</th><th>เลขที่ประจำตัว</th><th>จังหวัดภูมิลำเนา</th></tr>";
     $name = $row["std_name"];
